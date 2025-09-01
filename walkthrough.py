27872b24b5b282113ebae6a2b177500a8a98070c
Rebanto Nath <rebanto.nath@gmail.com>
2025-08-31 21:30:12 -0400

    implement walkthrough

---
+++ walkthrough.py
@@ -0,0 +1,35 @@
+import random
+
+# directed graph to model states and probabilities
+graph = {
+    'A': [0.2, 0.35, 0.45],
+    'B': [0, 0.4, 0.6],
+    'C': [0.3, 0.15, 0.25]
+}
+
+# params for markov chain
+state = 'A'
+steps = 2
+
+states = [state] # list to store the states through the steps
+
+for i in range(steps - 1):
+    choice = random.choices([0, 1, 2], graph[state], k=1)[0] # weighted random choice from current state
+    if choice == 0:
+        state = 'A'
+    elif choice == 1:
+        state = 'B'
+    else:
+        state = 'C'
+    states.append(state)
+
+for i in range(len(states) - 1):
+    print(f"{states[i]} -> ", end="")
+print(states[-1])
+
+# calculate probabilities of each state for stationary distribution
+p_a = states.count('A') / len(states)
+p_b = states.count('B') / len(states)
+p_c = states.count('C') / len(states)
+
+print(f"\nStationary Distrbution:\nState A = {p_a}\nState B = {p_b}\nState C = {p_c}")
